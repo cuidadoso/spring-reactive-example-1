d3065012a036a29f471184c0aee7b91f7a072880
Alexander Pyreev <a.s.pyreev@gmail.com>
2018-07-10 13:00:13 +0300

    Fix deserialization

--- src/main/java/com/alpere/reactiveexample1/Hello.java
+++ src/main/java/com/alpere/reactiveexample1/Hello.java
@@ -2,7 +2,13 @@ package com.alpere.reactiveexample1;
 
 import lombok.Data;
 
+import java.beans.ConstructorProperties;
+
 @Data
 public class Hello {
+    @ConstructorProperties("name")
+    Hello(String name) {
+        this.name = name;
+    }
     private final String name;
 }
